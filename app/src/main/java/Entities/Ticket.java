02eb267d4c2b664518988899a1ab82cd496f524d
Pankaj Bali <pankajbali0781@gmail.com>
2024-03-02 22:13:46 +0530

    Entities are updated

--- app/src/main/java/Entities/Ticket.java
+++ app/src/main/java/Entities/Ticket.java
@@ -1,4 +1,12 @@
 package Entities;
 
+import java.util.Date;
+
 public class Ticket {
+    private String ticketId;
+    private String userId;
+    private String source;
+    private String destination;
+    private Date dateOfTravel;
+    private Train train;
 }
--- app/src/main/java/Entities/Users.java
+++ app/src/main/java/Entities/Users.java
@@ -1,4 +1,11 @@
 package Entities;
 
+import java.util.List;
+
 public class Users {
+    private String name;
+    private String password;
+    private String hashedPassword;
+    private List<Ticket> ticketsBooked;
+    private String userId;
 }
